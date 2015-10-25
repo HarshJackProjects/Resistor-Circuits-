e9286b27f0754b20997f283cd977160955ed4db6
Harsh <93madon@gmail.com>
2015-10-25 18:42:06 -0400

    initial commit

---
+++ Resisters APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Resisters APP
+//
+//  Created by Harsh Patel on 10/25/15.
+//  Copyright © 2015 Harsh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
